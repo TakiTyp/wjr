40a7e34bcfc48a8c4b4803e799ed45eb5746b66c
klucha <takiekontomail@gmail.com>
2014-06-12 23:31:01 +0200

    style do menu; tooltipy; galeria plakatow

--- apps/frontend/modules/info/templates/showSuccess.php
+++ apps/frontend/modules/info/templates/showSuccess.php
@@ -8,4 +8,18 @@
     <hr/>
     <p><?php echo $wjr_info->getRaw('description') ?></p>
     <div class="clear"></div>
+	<?php if ($wjr_info->getId() == 1): ?>
+		<h2>Nocne Ujezdzanie Meridy</h2>
+		<hr>
+		<div id="POSTER_GALLERY">
+			<img class="poster" alt="Nocne Ujezdzanie Meridy I" title="Nocne Ujezdzanie Meridy I" src="uploads/plakat_nocne_ujezdzanie_meridy.jpg">
+			<img class="poster" alt="Nocne Ujezdzanie Meridy II" title="Nocne Ujezdzanie Meridy II" src="uploads/plakat_nocne_ujezdzanie_meridy_II.jpg">
+			<img class="poster" alt="Nocne Ujezdzanie Meridy III" title="Nocne Ujezdzanie Meridy III" src="uploads/plakat_nocne_ujezdzanie_meridy_III.jpg">
+			<img class="poster" alt="Nocne Ujezdzanie Meridy IV" title="Nocne Ujezdzanie Meridy IV" src="uploads/plakat_nocne_ujezdzanie_meridy_IV.jpg">
+		</div>
+		<a href="<?php echo url_for('@ujezdzanie') ?>">Zobacz wiêcej</a>
+	<?php endif; ?>
+	<?php if ($wjr_info->getId() == 5): ?>
+		<?php include_partial('gallery', array('gallery' => $gallery)) ?>
+	<?php endif; ?>
   </div>
